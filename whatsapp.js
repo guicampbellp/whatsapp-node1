c6660b583a27a116df072d42b2c34e7b01b9b2d0
guicampbellp <guilhermecampbell64@gmail.com>
2025-05-14 21:48:46 -0300

    whatsapp

--- whatsapp.js
+++ whatsapp.js
@@ -36,7 +36,7 @@ require("dotenv").config();
 
     // Configurações para evitar detecção
     await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
     );
     await page.evaluateOnNewDocument(() => {
       Object.defineProperty(navigator, "webdriver", {
