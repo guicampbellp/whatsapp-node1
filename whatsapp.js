64c773e29817721f3039dcf420d5587b4ab42c3f
guicampbellp <guilhermecampbell64@gmail.com>
2025-05-14 23:35:25 -0300

    dasdaw

--- whatsapp.js
+++ whatsapp.js
@@ -15,15 +15,17 @@ require("dotenv").config();
 
     // Configuração do Puppeteer
     console.log("Iniciando navegador...");
-    browser = await puppeteer.launch({
-      headless: true,
-   ignoreDefaultArgs: ["--disable-extensions"],
-   args:[
-    "--no-sandbox",
-    "--use-gl=egl",
-    "--disable-setuid-sandbox",
-   ],
-   ignoreHTTPSErrors:true,
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
